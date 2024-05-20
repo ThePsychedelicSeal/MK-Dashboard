cc297dbd18157cac7a2af84d4a9eff2e0bf60caf
PsychedelicSeal <trevorlrichfield@gmail.com>
2024-05-20 11:25:18 -0600

    Adding jQuery

--- js/mk8dx-data-collector.js
+++ js/mk8dx-data-collector.js
@@ -1,3 +1,45 @@
+$('.course-thumbnail').hide(); 
+$('#snesConsole').click(function (){
+    $('.course-thumbnail').not(':eq(.snesCourses)').hide();
+    $('.snesCourses').show();
+})
+$('#n64Console').click(function (){
+    $('.course-thumbnail').not(':eq(.n64Courses)').hide();
+    $('.n64Courses').show();
+})
+$('#gbaConsole').click(function (){
+    $('.course-thumbnail').not(':eq(.gbaCourses)').hide();
+    $('.gbaCourses').show();
+})
+$('#gcnConsole').click(function (){
+    $('.course-thumbnail').not(':eq(.gcnCourses)').hide();
+    $('.gcnCourses').show();
+})
+$('#dsConsole').click(function (){
+    $('.course-thumbnail').not(':eq(.dsCourses)').hide();
+    $('.dsCourses').show();
+})
+$('#wiiConsole').click(function (){
+    $('.course-thumbnail').not(':eq(.wiiCourses)').hide();
+    $('.wiiCourses').show();
+})
+$('#mk7Console').click(function (){
+    $('.course-thumbnail').not(':eq(.mk7Courses)').hide();
+    $('.mk7Courses').show();
+})
+$('#tourConsole').click(function (){
+    $('.course-thumbnail').not(':eq(.tourCourses)').hide();
+    $('.tourCourses').show();
+})
+$('#mk8Console').click(function (){
+    $('.course-thumbnail').not(':eq(.mk8Courses)').hide();
+    $('.mk8Courses').show();
+})
+$('#boosterConsole').click(function (){
+    $('.course-thumbnail').not(':eq(.boosterCourses)').hide();
+    $('.boosterCourses').show();
+})
+
 /* function snesCourses(){
     document.getElementById('snesCourses').style.display = 'grid';
     document.getElementById('snesCourses').style.gridTemplateColumns = 'repeat(5, 1fr)'; 
