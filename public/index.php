293043d0f2433ab49a2f6f9b6551f138ab5e46af
ThePsychedelicSeal <trevorlrichfield@gmail.com>
2024-05-23 08:21:32 -0600

    Adding PHP

---
+++ public/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
+
+<script src="index.js"></script>
+<?php
+$dirCoursePath = "../images/courses";
+$courseFiles = scandir($dirCoursePath);
+$hello = "hello";
+
+foreach ($courseFiles as $courseFile) {
+    $filePath = $dirCoursePath . '/' . $courseFile;
+/*     if (is_file($filePath)) {
+        echo $courseFile . "<br>";
+    } */
+    
+    echo "label";
+};
+
+?>
