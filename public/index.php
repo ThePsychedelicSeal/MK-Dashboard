19efb3cca08206290b1ba6c75fae558999ef22bc
PsychedelicSeal <trevorlrichfield@gmail.com>
2024-05-24 01:42:21 -0600

    Reformatting /root and adding Composer

--- public/index.php
+++ public/index.php
@@ -1,13 +1,23 @@
+<?php
+const BASE_PATH = __DIR__ . '/../';
+require BASE_PATH . '/vendor/autoload.php';
+?>
+
+
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
-    
+
 </body>
+
 </html>
 
 <script src="index.js"></script>
@@ -18,10 +28,10 @@ $hello = "hello";
 
 foreach ($courseFiles as $courseFile) {
     $filePath = $dirCoursePath . '/' . $courseFile;
-/*     if (is_file($filePath)) {
+    /*     if (is_file($filePath)) {
         echo $courseFile . "<br>";
     } */
-    
+
     echo "label";
 };
 
